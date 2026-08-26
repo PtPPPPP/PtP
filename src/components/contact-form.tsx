47d66c2afeaad816b75bf0c52841ca3011b14bcc
PtPPPPP <1074897559@qq.com>
2026-08-27 02:38:05 +0800

    chore(cleanup): remove orphan ContactForm component, test and related styles

--- src/components/contact-form.tsx
+++
@@ -1,8 +0,0 @@
-export function ContactForm() {
-  return (
-    <div className="contact-form contact-form--disabled" role="note">
-      <strong>联系表单暂未启用</strong>
-      <p>当前没有后端发送服务，也不会收集输入内容。请使用左侧 GitHub 入口联系。</p>
-    </div>
-  );
-}
